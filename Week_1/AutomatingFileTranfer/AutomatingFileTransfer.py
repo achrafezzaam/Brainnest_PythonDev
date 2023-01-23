72d988e3ad3189302b14b1ada26b775ad8cd4ebe
achrafezzaam <achraf.ezzaam@gmail.com>
2023-01-23 20:54:42 +0100

    Doing the tasks for the file automation project. The program is still missing the loging part

--- Week_1/AutomatingFileTranfer/AutomatingFileTransfer.py
+++ Week_1/AutomatingFileTranfer/AutomatingFileTransfer.py
@@ -1,20 +1,70 @@
-''' You work at a company that receives daily data files from external partners. These files need to be processed and analyzed, but first, they need to be transferred to the company's internal network.
-
-The goal of this project is to automate the process of transferring the files from an external FTP server to the company's internal network.
-
-Here are the steps you can take to automate this process:
-
-    Use the ftplib library to connect to the external FTP server and list the files in the directory.
-
-    Use the os library to check for the existence of a local directory where the files will be stored.
-
-    Use a for loop to iterate through the files on the FTP server and download them to the local directory using the ftplib.retrbinary() method.
-
-    Use the shutil library to move the files from the local directory to the internal network.
-
-    Use the schedule library to schedule the script to run daily at a specific time.
-
-    You can also set up a log file to keep track of the files that have been transferred and any errors that may have occurred during the transfer process. '''
-
-
-
+''' You work at a company that receives daily data files from external partners. These files need to be processed and analyzed, but first, they need to be transferred to the company's internal network.
+
+
+
+The goal of this project is to automate the process of transferring the files from an external FTP server to the company's internal network.
+
+
+
+Here are the steps you can take to automate this process:
+
+
+
+    Use the ftplib library to connect to the external FTP server and list the files in the directory.
+
+
+
+    Use the os library to check for the existence of a local directory where the files will be stored.
+
+
+
+    Use a for loop to iterate through the files on the FTP server and download them to the local directory using the ftplib.retrbinary() method.
+
+
+
+    Use the shutil library to move the files from the local directory to the internal network.
+
+
+
+    Use the schedule library to schedule the script to run daily at a specific time.
+
+
+
+    You can also set up a log file to keep track of the files that have been transferred and any errors that may have occurred during the transfer process. '''
+
+from ftplib import FTP, error_perm
+import shutil
+from os import getcwd, mkdir
+from os.path import join, exists
+import schedule
+
+internal_net = "Don't know what it is but enter it here"
+
+host = input("Write your host name then press enter: ")
+user = input("Write your username then press enter: ")
+passwd = input("Write your password then press enter: ")
+
+ftp = FTP(host,user,passwd)
+
+files_list = []
+
+try:
+    files = ftp.nlst()
+except error_perm as resp:
+    if str(resp) == "550 No files found":
+        print("No files in this directory")
+    else:
+        raise
+
+ftp.quit()
+
+dir_path = join(getcwd(),"file_store")
+if not exists(dir_path):
+    mkdir(dir_path)
+
+for file in files_list:
+    local_file = join(dir_path,file)
+    ftp.retrbinary(file, open(local_file, 'wb').write)
+    shutil.copyfile(local_file, internal_net)
+
+schedule.every().day.at("00:00").do()
